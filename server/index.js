e0993082e270941b59f560c609703b0935c6b087
PranayMohan1 <37791489+PranayMohan1@users.noreply.github.com>
2021-05-02 17:50:53 +0530

    yes_update
    removed credentials

--- server/index.js
+++ server/index.js
@@ -14,7 +14,7 @@ app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://mohanpranay22:Gogoagone@1@cluster0.nrlxb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+const CONNECTION_URL = 
 const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
